27f936bfc7fb5559c4dc445a9301f8272b7f9fe4
Latsuj <justal.kevin@gmail.com>
2021-06-14 20:02:11 +0800

    [FEATURE] Adding the react spring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Home from './Pages/Home'
+import { useTransition, animated } from 'react-spring'
 
 function App() {
+  const location = useLocation()
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 1 },
+    enter: { opacity: 1},
+    leave: { opacity: 0 },
+    mass: 1,
+    tension: 210,
+    friction: 20
+  })
+
   return (
-    <Router>
-      <div className="content">
+    <div className="content">
+    {transitions.map(({ item, props, key }) => (
+      <animated.div key={key} style={props}>
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
-      </div>
-    </Router>
+      </animated.div>
+    ))}
+    </div>
   );
 }
 
